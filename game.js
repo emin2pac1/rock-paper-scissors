46240814d74c3a3119c19a61078dcf4838d53835
Milad <emintowpac@gmail.com>
2021-07-29 12:10:23 +0300

    Pushed All Changes to main Branch

--- game.js
+++ game.js
@@ -20,44 +20,44 @@ startButton.addEventListener('click' , function () {
         enableElement.removeAttribute("disabled");
         
     });
-
-        display.textContent ="choose a weapon";
-
     
-  
+    display.textContent ="choose a weapon";
+    
+    
+    
 });
 
-    if (playerWins == 5 || computerWins ==5)
-    gameRound();
+if (playerWins == 5 || computerWins ==5)
+gameRound();
 
-    getChoice.forEach( function(choice){
-        choice.addEventListener("click", function(){
-              playerChoice= choice.value;
-             if (playerChoice) {
-                 getChoice.forEach( btn =>{
-                     btn.setAttribute("disabled", "disabled");
-                 });
-                gameRound();
-             } 
-
-        } );
+getChoice.forEach( function(choice){
+    choice.addEventListener("click", function(){
+        playerChoice= choice.value;
+        if (playerChoice) {
+            getChoice.forEach( btn =>{
+                btn.setAttribute("disabled", "disabled");
+            });
+            gameRound();
+        } 
         
-    });
-
+    } );
     
+});
+
+
 
 
 function computerPlay () {
     let randComputerChoice = Math.floor((Math.random() * 100) + 1);
     let computerChoiceResult;
     if (randComputerChoice <= 20)
-        computerChoiceResult = "Paper".toUpperCase();
-        else if (randComputerChoice >= 20 && randComputerChoice <= 60)
-        computerChoiceResult = "Rock".toUpperCase();
-        else if (randComputerChoice > 60)
-        computerChoiceResult = "Scissors".toUpperCase();
-        return computerChoiceResult;
-
+    computerChoiceResult = "Paper".toUpperCase();
+    else if (randComputerChoice >= 20 && randComputerChoice <= 60)
+    computerChoiceResult = "Rock".toUpperCase();
+    else if (randComputerChoice > 60)
+    computerChoiceResult = "Scissors".toUpperCase();
+    return computerChoiceResult;
+    
 }
 
 
@@ -65,102 +65,102 @@ function gameRound (){
     const computerFinalResult = computerPlay();
     const playerFinalResult = playerChoice.toUpperCase();
     
-        if(playerFinalResult == null)
-        return;
+    if(playerFinalResult == null)
+    return;
+    if(playerWins<5 && computerWins<5)
+    display.textContent  = `Player Chose ${playerFinalResult}`;
+    
+    setTimeout(() => {
         if(playerWins<5 && computerWins<5)
-        display.textContent  = `Player Chose ${playerFinalResult}`;
-
+        display.textContent  =`Computer Chose ${computerFinalResult}`;
+    }, 2000);
+    
+    
+    if (playerFinalResult == computerFinalResult) {
+        ++ties;
+        
+        setTimeout(() => {
+            display.textContent  =`It's a TIE`;
+        }, 4000);
+        
+    }
+    else if (  (playerFinalResult == 'ROCK' && computerFinalResult == 'SCISSORS')
+    || (playerFinalResult == 'PAPER' && computerFinalResult == 'ROCK') 
+    || (playerFinalResult == 'SCISSORS' && computerFinalResult == 'PAPER') ) {
+        ++playerWins;
         setTimeout(() => {
             if(playerWins<5 && computerWins<5)
-            display.textContent  =`Computer Chose ${computerFinalResult}`;
+            display.textContent  =`PLAYER wins`;
+        }, 4000);
+        
+    }
+    
+    else {
+        ++computerWins;
+        setTimeout(() => {
+            if(playerWins<5 && computerWins<5)
+            display.textContent  =`COMPUTER wins`;
+        }, 4000);
+    }
+    
+    setTimeout(() => {
+        display.textContent  =`Player: ${playerWins} && Computer: ${computerWins}`;
+    }, 6000);
+    
+    if (playerWins == 5 || computerWins ==5 ) {
+        
+        allButtons.forEach(disableElement => {
+            
+            disableElement.setAttribute("disabled", "disabled");
+            
+        });
+        
+        display.textContent = "Game Over!";
+        
+        setTimeout(() => {
+            display.textContent  = `Player Chose ${playerFinalResult}`;    
         }, 2000);
-
         
-        if (playerFinalResult == computerFinalResult) {
-            ++ties;
-
-            setTimeout(() => {
-                display.textContent  =`It's a TIE`;
-            }, 4000);
-    
-        }
-        else if (  (playerFinalResult == 'ROCK' && computerFinalResult == 'SCISSORS')
-                 || (playerFinalResult == 'PAPER' && computerFinalResult == 'ROCK') 
-                || (playerFinalResult == 'SCISSORS' && computerFinalResult == 'PAPER') ) {
-                ++playerWins;
-                 setTimeout(() => {
-                     if(playerWins<5 && computerWins<5)
-                    display.textContent  =`PLAYER wins`;
-                 }, 4000);
-
-            }
-
-            else {
-                ++computerWins;
-                setTimeout(() => {
-                    if(playerWins<5 && computerWins<5)
-                    display.textContent  =`COMPUTER wins`;
-                }, 4000);
-            }
-
-            setTimeout(() => {
-                display.textContent  =`Player: ${playerWins} && Computer: ${computerWins}`;
-            }, 6000);
-
-         if (playerWins == 5 || computerWins ==5 ) {
-
-            allButtons.forEach(disableElement => {
-
-                disableElement.setAttribute("disabled", "disabled");
         
-             });
+        setTimeout(() => {
+            display.textContent  =`Computer Chose ${computerFinalResult}`;
+        }, 4000);
         
-            display.textContent = "Game Over!";
-
-                setTimeout(() => {
-                    display.textContent  = `Player Chose ${playerFinalResult}`;    
-                }, 2000);
-
-
-                setTimeout(() => {
-                    display.textContent  =`Computer Chose ${computerFinalResult}`;
-                }, 4000);
-                       
-                setTimeout(() => {
-                    if(playerWins > computerWins) {
-        
-                        display.textContent="Player Won the Game";
-        
-                    }
-        
-                    if (computerWins> playerWins) {
-                        display.textContent="Computer Won the Game";
-                    }
-                    
-                    },6000);
+        setTimeout(() => {
+            if(playerWins > computerWins) {
                 
+                display.textContent="Player Won the Game";
                 
-                return;
             }
+            
+            if (computerWins> playerWins) {
+                display.textContent="Computer Won the Game";
+            }
+            
+        },6000);
+        
+        
+        return;
+    }
     
-
-            setTimeout(() => {
-                display.textContent  =`Another Round?`;
-                replayButton.removeAttribute('disabled');
-            }, 8000);            
-
-            replayButton.addEventListener('click', function () {
-
-                getChoice.forEach(enableElement => {
-                    enableElement.removeAttribute("disabled");
-
-                    
-                });
-
-                setTimeout(() => {
-                    replayButton.setAttribute("disabled","disabled");
-                    display.textContent="Choose a Weapon";
-                }, 100);
-            });
-
-            }
+    
+    setTimeout(() => {
+        display.textContent  =`Another Round?`;
+        replayButton.removeAttribute('disabled');
+    }, 8000);            
+    
+    replayButton.addEventListener('click', function () {
+        
+        getChoice.forEach(enableElement => {
+            enableElement.removeAttribute("disabled");
+            
+            
+        });
+        
+        setTimeout(() => {
+            replayButton.setAttribute("disabled","disabled");
+            display.textContent="Choose a Weapon";
+        }, 100);
+    });
+    
+}
